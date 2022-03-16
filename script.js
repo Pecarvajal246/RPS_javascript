ed2109ba5c9c83ac1b144b633cee48e8340b4929
pedro <Pecarvajal246@gmail.com>
2022-03-16 14:52:01 -0400

    RPS UI

--- script.js
+++ script.js
@@ -3,17 +3,19 @@ function computerPlay() {
 	selection=Math.floor(Math.random()*3)
 	return rps[selection]
 }
-function playRound(playerSelection, computerSelection) {
+// function playRound(e) {
+function playRound(e) {
+	playerSelection =e.target.classList.value;
 	const ps = playerSelection.toLowerCase();
-	const cs = computerSelection.toLowerCase();
+	const cs = computerPlay().toLowerCase();
 	if (ps==='rock' && cs ==='scissors') {
 		return 'You win! Rock beats Scissors!'
 	}
 	else if (ps === 'scissors' && cs === 'paper') {
-		return 'You win! Scissors beats Paper!'	
+		return 'You win! Scissors beats Paper!'
 	}
 	else if (ps === 'paper' && cs === 'rock') {
-		return 'You win! Paper beats Rock!'	
+		return 'You win! Paper beats Rock!'
 	}
 	else if (ps === 'rock' && cs === 'paper') {
 		return 'You lose! Paper beats Rock!'
@@ -22,34 +24,65 @@ function playRound(playerSelection, computerSelection) {
 		return 'You lose! Rock beats Scissors!'
 	}
 	else if (cs === 'scissors' && ps === 'paper') {
-		return 'You win! Scissors beats Paper!'	
+		return 'You win! Scissors beats Paper!'
 	}
 	else return 'Draw! '+ ps+ " doesn't beat " + cs
 }
-function game() {
-	let playerScore=0
-	let computerScore=0
-	
-	for (let i = 0; i < 5; i++) {
-		let playerSelection = window.prompt('Enter: Rock, Paper or Scissors');
-		let result = playRound(playerSelection,computerPlay());
-		console.log(result)
+function game(e) {
+	if (finishedGame === false){
+		let result = playRound(e);
+		div.textContent = result
 
 		if (result.startsWith('You win!')) {
 			playerScore++;
 			console.log('p '+playerScore)
+			div2.textContent = "Player Score "+playerScore + " Computer Score "+ computerScore
 		}
 		else if (result.startsWith('You lose!')) {
 			computerScore++
 			console.log('c '+computerScore)
+			div2.textContent = "Player Score "+playerScore + " Computer Score "+ computerScore
+		}
+		if (playerScore === 5) {
+			div.textContent = 'You win the game!'
+			finishedGame = true
+			const button = document.createElement('button')
+			body.appendChild(button)
+			button.textContent = 'Play again'
+			button.addEventListener('click', function (e){
+				playerScore=0
+				computerScore=0
+				finishedGame=false
+				div.textContent = ''
+				div2.textContent = "Player Score 0 Computer Score 0"
+				body.removeChild(button)
+			})
+		}
+		else if (computerScore === 5) {
+			div.textContent ='You lose! try again!'
+			finishedGame = true
+			const button = document.createElement('button')
+			body.appendChild(button)
+			button.textContent = 'Play again'
+			button.addEventListener('click', function (e){
+				playerScore=0
+				computerScore=0
+				finishedGame=false
+				div.textContent = ''
+				div2.textContent = "Player Score 0 Computer Score 0"
+				body.removeChild(button)
+			})
 		}
 	}
 
-	if (playerScore > computerScore) {
-		return 'You win the game!'
-	}
-	else if (computerScore > playerScore) {
-		return 'You lose! try again!'
-	}
 }
-console.log(game())
+let playerScore=0
+let computerScore=0
+let finishedGame = false
+const body = document.querySelector('body')
+const div = document.createElement('div')
+body.appendChild(div)
+const div2 = document.createElement('div2')
+body.appendChild(div2)
+const buttons = document.querySelectorAll('button')
+buttons.forEach(button =>button.addEventListener('click', game));
